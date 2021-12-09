7d73adab191116adc9a8ab4759a2946b87ae20c8
Hyunwoo Jung <chaosphch@gmail.com>
2021-12-09 20:41:35 +0900

    Refactor

--- Pixel_4/client.py
+++ Pixel_4/client.py
@@ -4,10 +4,11 @@ import os
 import time
 import socket
 import argparse
-import matplotlib.pyplot as plt
 import csv
+from collections import namedtuple
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 from SurfaceFlinger.get_fps import SurfaceFlingerFPS, get_multi_dnn_profiler_fps
 from PowerLogger.powerlogger import PowerLogger
@@ -16,93 +17,211 @@ from GPU.gpu import GPU, gpu_clock_list
 
 
 FILEPATH_STATES = 'states.csv'
+SURFACE_FLINGER_TARGETS = {
+    'showroom': '"SurfaceView - com.android.chrome/com.google.android.apps.chrome.Main#0"',
+    'skype': '"com.skype.raider/com.skype4life.MainActivity#0"',
+    'call': '"SurfaceView - com.tencent.tmgp.kr.codm/com.tencent.tmgp.cod.CODMainActivity#0"',
+    'multi_dnn_profiler': '"com.multi_dnn_profiler/com.multi_dnn_profiler.MainActivity#0"',
+    #"\"SurfaceView - com.tencent.tmgp.kr.codm/com.tencent.tmgp.cod.CODMainActivity#0\""
+    #"\"com.skype.raider/com.skype4life.MainActivity#0\""
+    #"\"SurfaceView - com.android.chrome/org.chromium.chrome.browser.ChromeTabbedActivity#0\""
+}
+
+# (CPU clock, GPU clock, power, CPU temperature, GPU temperature)
+State = namedtuple('State', ['c_c', 'g_c', 'c_p', 'c_t', 'g_t', 'fps'])
 
 
-def log_results(pl, fps_data, c0, c6, g):
-    # Logging results
-    print('Average Total power={} mW'.format(sum(pl.power_data) / len(pl.power_data)))
-    print('Average fps = {} fps'.format(sum(fps_data) / len(fps_data)))
+class Client:
 
-    ts = range(0, len(fps_data))
-    f = open('power_skype_zTT.csv', 'w', encoding='utf-8', newline='')
-    wr = csv.writer(f)
-    wr.writerow(pl.power_data[1:])
-    f.close()
+    def __init__(self, app, exp_time, server_ip, server_port, target_fps, pixel_ip):
+        self.app = app
+        self.exp_time = exp_time
+        self.server_ip = server_ip
+        self.server_port = server_port
+        self.target_fps = target_fps
+        self.pixel_ip = pixel_ip
+        self.pl = PowerLogger()
+        self.t = 0
+        self.ts = []
+        self.fps_data = []
+        self.state = State(c_c=0, g_c=0, c_p=0, c_t=0, g_t=0, fps=0)
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.connect((self.server_ip, self.server_port))
+        self.c0 = CPU(0, ip=self.pixel_ip, cpu_type='l')
+        self.c6 = CPU(4, ip=self.pixel_ip, cpu_type='b')
+        self.g = GPU(ip=self.pixel_ip)
+        self.start_time = None
+        self.initialize()
 
-    f = open('temp_skype_zTT.csv', 'w', encoding='utf-8', newline='')
-    wr = csv.writer(f)
-    wr.writerow(c0.temp_data)
-    wr.writerow(c6.temp_data)
-    wr.writerow(g.temp_data)
-    f.close()
+    def initialize(self):
+        ''' Set CPU and GPU governor to userspace '''
+        self.c0.setUserspace()
+        self.c6.setUserspace()
+        # self.g.setUserspace()
 
-    f = open('clock_skype_zTT.csv', 'w', encoding='utf-8', newline='')
-    wr = csv.writer(f)
-    wr.writerow(c0.clock_data)
-    wr.writerow(c6.clock_data)
-    wr.writerow(g.clock_data)
-    f.close()
+        ''' Set CPU and GPU clock to maximum before starting '''
+        max_clock_index_cpu = len(big_cpu_clock_list) - 1
+        max_clock_index_gpu = len(gpu_clock_list) - 1
+        self.c0.setCPUclock(max_clock_index_cpu)
+        self.c6.setCPUclock(max_clock_index_cpu)
+        self.g.setGPUclock(2)
+        
+        if self.app != 'multi_dnn_profiler':
+            view = SURFACE_FLINGER_TARGETS[self.app]
+            self.sf_fps_driver = SurfaceFlingerFPS(view, ip=self.pixel_ip)
 
-    f = open('fps_skype_zTT.csv', 'w', encoding='utf-8', newline='')
-    wr = csv.writer(f)
-    wr.writerow(fps_data)
-    f.close()
+        c_c = max_clock_index_cpu
+        g_c = max_clock_index_gpu
+        c_t = float(self.c0.getCPUtemp())
+        g_t = float(self.g.getGPUtemp())
+        fps = self.get_fps()
+        self.state = State(c_c=c_c, g_c=g_c, c_p=int(self.pl.getPower() / 100), c_t=c_t, g_t=g_t, fps=fps)
+        time.sleep(4)
 
-    # Plot results
-    fig = plt.figure(figsize=(12, 14))
-    ax1 = fig.add_subplot(2, 2, 1)
-    ax2 = fig.add_subplot(2, 2, 2)
-    ax3 = fig.add_subplot(2, 2, 3)
-    ax4 = fig.add_subplot(2, 2, 4)
+        if os.path.exists(FILEPATH_STATES):
+            os.remove(FILEPATH_STATES)
 
-    ax1.set_xlabel('time')
-    ax1.set_ylabel('power(mw)')
-    ax1.set_ylim(0, 1.1 * np.max(pl.power_data))
-    ax1.grid(True)
-    ax1.plot(ts, pl.power_data[1:], label='TOTAL')
-    ax1.legend(loc='upper right')
-    ax1.set_title('Power')
+    def run(self):
+        self.start_time = time.time()
+        while self.t < self.exp_time:
+            self.step()
+            self.t += 1
 
-    ax2.set_xlabel('time')
-    ax2.set_ylabel('temperature')
-    ax2.grid(True)
-    ax2.plot(ts, c0.temp_data, label='LITTLE')
-    ax2.plot(ts, c6.temp_data, label='Big')
-    ax2.plot(ts, g.temp_data, label='GPU')
-    ax2.legend(loc='upper right')
-    ax2.set_title('temperature')
+    def step(self):
+        send_msg = str(tuple(self.state))[1:-1]
+        self.client_socket.send(send_msg.encode())
+        is_received = False
+        while not is_received:
+            try:
+                recv_msg = self.client_socket.recv(8702).decode()
+                clk = recv_msg.split(',')
+                c_c = int(clk[0])
+                g_c = int(clk[1])
+                is_received = True
+            except:
+                pass
 
-    ax3.set_xlabel('time')
-    ax3.set_ylabel('clock frequency(MHz)')
-    ax3.set_ylim(0, 1.1 * np.max([np.max(c0.clock_data), np.max(c6.clock_data), np.max(g.clock_data)]))
-    ax3.grid(True)
-    ax3.plot(ts, c0.clock_data, label='LITTLE')
-    ax3.plot(ts, c6.clock_data, label='Big')
-    ax3.plot(ts, g.clock_data, label='GPU')
-    ax3.legend(loc='upper right')
-    ax3.set_title('clock')
+        self.c0.setCPUclock(c_c)
+        self.c6.setCPUclock(c_c)
+        self.g.setGPUclock(g_c)
 
-    ax4.set_xlabel('time')
-    ax4.set_ylabel('FPS')
-    ax4.set_ylim(0, 1.1 * np.max([np.max(fps_data), target_fps]))
-    ax4.grid(True)
-    ax4.plot(ts, fps_data, label='Average FPS')
-    ax4.axhline(y=target_fps, color='r', linewidth=1)
-    ax4.legend(loc='upper right')
-    ax4.set_title('fps')
+        fps = self.get_fps()
+        self.fps_data.append(fps)
 
-    plt.tight_layout
-    plt.show()
+        self.ts.append(self.t)
+
+        self.c0.collectdata()
+        self.c6.collectdata()
+        self.g.collectdata()
+
+        c_p = int(self.pl.getPower() / 100)
+        if c_p == 0:
+            return
+
+        c_t = float(self.c0.getCPUtemp())
+        g_t = float(self.g.getGPUtemp())
+
+        next_state = State(c_c, g_c, c_p, c_t, g_t, fps)
+        print('[{}] state:{} next_state:{} fps:{}'.format(self.t, self.state, next_state, fps))
+        self.state = next_state
+
+        with open(FILEPATH_STATES, 'a') as fp:
+            fp.write(','.join(map(str, [time.time() - self.start_time] + list(self.state))) + '\n')
+
+    def get_fps(self):
+        if self.app == 'multi_dnn_profiler':
+            fps = get_multi_dnn_profiler_fps(self.pixel_ip)
+        else:
+            fps = float(self.sf_fps_driver.getFPS())
+        if fps > 60:
+            fps = 60.0
+        return fps
+
+    def log_results(self):
+        # Logging results
+        print('Average Total power={} mW'.format(sum(self.pl.power_data) / len(self.pl.power_data)))
+        print('Average fps = {} fps'.format(sum(self.fps_data) / len(self.fps_data)))
+
+        ts = range(0, len(self.fps_data))
+        f = open('power_skype_zTT.csv', 'w', encoding='utf-8', newline='')
+        wr = csv.writer(f)
+        wr.writerow(self.pl.power_data[1:])
+        f.close()
+
+        f = open('temp_skype_zTT.csv', 'w', encoding='utf-8', newline='')
+        wr = csv.writer(f)
+        wr.writerow(self.c0.temp_data)
+        wr.writerow(self.c6.temp_data)
+        wr.writerow(self.g.temp_data)
+        f.close()
+
+        f = open('clock_skype_zTT.csv', 'w', encoding='utf-8', newline='')
+        wr = csv.writer(f)
+        wr.writerow(self.c0.clock_data)
+        wr.writerow(self.c6.clock_data)
+        wr.writerow(self.g.clock_data)
+        f.close()
+
+        f = open('fps_skype_zTT.csv', 'w', encoding='utf-8', newline='')
+        wr = csv.writer(f)
+        wr.writerow(self.fps_data)
+        f.close()
+
+        # Plot results
+        fig = plt.figure(figsize=(12, 14))
+        ax1 = fig.add_subplot(2, 2, 1)
+        ax2 = fig.add_subplot(2, 2, 2)
+        ax3 = fig.add_subplot(2, 2, 3)
+        ax4 = fig.add_subplot(2, 2, 4)
+
+        ax1.set_xlabel('time')
+        ax1.set_ylabel('power(mw)')
+        ax1.set_ylim(0, 1.1 * np.max(self.pl.power_data))
+        ax1.grid(True)
+        ax1.plot(ts, self.pl.power_data[1:], label='TOTAL')
+        ax1.legend(loc='upper right')
+        ax1.set_title('Power')
+
+        ax2.set_xlabel('time')
+        ax2.set_ylabel('temperature')
+        ax2.grid(True)
+        ax2.plot(ts, self.c0.temp_data, label='LITTLE')
+        ax2.plot(ts, self.c6.temp_data, label='Big')
+        ax2.plot(ts, self.g.temp_data, label='GPU')
+        ax2.legend(loc='upper right')
+        ax2.set_title('temperature')
+
+        ax3.set_xlabel('time')
+        ax3.set_ylabel('clock frequency(MHz)')
+        ax3.set_ylim(0, 1.1 * np.max([np.max(self.c0.clock_data), np.max(self.c6.clock_data), np.max(self.g.clock_data)]))
+        ax3.grid(True)
+        ax3.plot(ts, self.c0.clock_data, label='LITTLE')
+        ax3.plot(ts, self.c6.clock_data, label='Big')
+        ax3.plot(ts, self.g.clock_data, label='GPU')
+        ax3.legend(loc='upper right')
+        ax3.set_title('clock')
+
+        ax4.set_xlabel('time')
+        ax4.set_ylabel('FPS')
+        ax4.set_ylim(0, 1.1 * np.max([np.max(self.fps_data), self.target_fps]))
+        ax4.grid(True)
+        ax4.plot(ts, self.fps_data, label='Average FPS')
+        ax4.axhline(y=self.target_fps, color='r', linewidth=1)
+        ax4.legend(loc='upper right')
+        ax4.set_title('fps')
+
+        plt.tight_layout
+        plt.show()
 
 
-if __name__ == "__main__":
+def parse_arguments():
     ''' 
-        --app            Application name [showroom, skype, call]
-        --exp_time         Time steps for learning
-        --server_ip        Agent server IP
-        --server_port    Agent server port
-        --target_fps    Target FPS
-        --pixel_ip        Pixel device IP for connecting device via adb
+    --app            Application name [showroom, skype, call]
+    --exp_time         Time steps for learning
+    --server_ip        Agent server IP
+    --server_port    Agent server port
+    --target_fps    Target FPS
+    --pixel_ip        Pixel device IP for connecting device via adb
     '''
     parser = argparse.ArgumentParser()
     parser.add_argument('--app', type=str, required=True, choices=['showroom', 'skype', 'call', 'multi_dnn_profiler'],
@@ -112,135 +231,11 @@ if __name__ == "__main__":
     parser.add_argument('--server_port', type=int, default=8702, help="Agent server port number")
     parser.add_argument('--target_fps', type=int, required=True, help="Target FPS")
     parser.add_argument('--pixel_ip', type=str, required=True, help="Pixel device IP for connecting device via adb")
+    return parser.parse_args()
 
-    args = parser.parse_args()
-    app = args.app
-    experiment_time = args.exp_time
-    server_ip = args.server_ip
-    server_port = args.server_port
-    target_fps = args.target_fps
-    pixel_ip = args.pixel_ip
-    
-    t = 0
-    ts = []
-    fps_data = []
 
-    ''' Connect to agent server '''
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((server_ip, server_port))
-
-    '''  
-        Create big, LITTLE and GPU instances 
-        For Pixel 3a, c0 -> LITTLE, c6 -> big, g -> GPU
-    '''
-    c0 = CPU(0, ip=pixel_ip, cpu_type='l')
-    c6 = CPU(4, ip=pixel_ip, cpu_type='b')
-    g = GPU(ip=pixel_ip)
-    pl = PowerLogger()
-
-    ''' Set CPU and GPU governor to userspace '''
-    c0.setUserspace()
-    c6.setUserspace()
-    # g.setUserspace()
-
-    ''' Set CPU and GPU clock to maximum before starting '''
-    c0.setCPUclock(17)
-    c6.setCPUclock(16)
-    g.setGPUclock(2)
-
-    ''' Check whether setting clocks is properly or not '''
-    c0.getCurrentClock()
-    c6.getCurrentClock()
-    g.getCurrentClock()
-
-    ''' Create fps driver '''
-    if app == 'showroom':
-        view = "\"SurfaceView - com.android.chrome/com.google.android.apps.chrome.Main#0\""
-    elif app == 'skype':
-        view = "\"com.skype.raider/com.skype4life.MainActivity#0\""
-    elif app == 'call':
-        view = "\"SurfaceView - com.tencent.tmgp.kr.codm/com.tencent.tmgp.cod.CODMainActivity#0\""
-    elif app == 'multi_dnn_profiler':
-        view = "\"com.multi_dnn_profiler/com.multi_dnn_profiler.MainActivity#0\""
-    
-    #view = "\"SurfaceView - com.tencent.tmgp.kr.codm/com.tencent.tmgp.cod.CODMainActivity#0\""
-    # view = "\"com.skype.raider/com.skype4life.MainActivity#0\""
-    #view = "\"SurfaceView - com.android.chrome/org.chromium.chrome.browser.ChromeTabbedActivity#0\""
-    
-    if app != 'multi_dnn_profiler':
-        sf_fps_driver = SurfaceFlingerFPS(view, ip=pixel_ip)
-    
-    ''' 
-        Set initial state
-        c_c -> CPU clock
-        g_c -> GPU clock
-        c_t -> CPU temperature
-        g_t -> GPU temperature
-    '''
-    c_c = len(big_cpu_clock_list) - 1
-    g_c = len(gpu_clock_list) - 1
-    c_t = float(c0.getCPUtemp())
-    g_t = float(g.getGPUtemp())
-    state = (c_c, g_c, int(pl.getPower() / 100), 0, c_t, g_t)
-    time.sleep(4)
-
-    if os.path.exists(FILEPATH_STATES):
-        os.remove(FILEPATH_STATES)
-
-    start_time = time.time()
-
-    ''' Start learning '''
-    while(1):
-        if app == 'multi_dnn_profiler':
-            fps = get_multi_dnn_profiler_fps(pixel_ip)
-        else:
-            fps = float(sf_fps_driver.getFPS())
-        if fps > 60:
-            fps = 60.0
-        fps_data.append(fps)
-        
-        ts.append(t)
-
-        c0.collectdata()
-        c6.collectdata()
-        g.collectdata()
-        
-        c_p = int(pl.getPower() / 100)
-        if c_p == 0:
-            continue
-        g_p = 0
-        # time.sleep(3.7)
-        
-        c_t = float(c0.getCPUtemp())
-        g_t = float(g.getGPUtemp())
-        next_state = (c_c, g_c, c_p, g_p, c_t, g_t, fps)
-
-        with open(FILEPATH_STATES, 'a') as fp:
-            fp.write(','.join(map(str, [time.time() - start_time] + list(next_state))) + '\n')
-        
-        send_msg = str(c_c) + ',' + str(g_c) + ',' + str(c_p) + ',' + str(g_p) + ',' \
-			+ str(c_t) + ',' + str(g_t) + ',' + str(fps)
-        client_socket.send(send_msg.encode())
-        print('[{}] state:{} next_state:{} fps:{}'.format(t, state, next_state, fps))
-        state = next_state
-        # get action
-        is_received = False
-        while not is_received:
-            try:
-                recv_msg = client_socket.recv(8702).decode()
-                clk = recv_msg.split(',')
-                c_c = int(clk[0])
-                g_c = int(clk[1])
-                is_received = True
-            except:
-                pass
-
-        c0.setCPUclock(c_c)
-        c6.setCPUclock(c_c)
-        g.setGPUclock(g_c)
-
-        t = t + 1
-        if t == experiment_time:
-            break
-
-    log_results(pl, fps_data, c0, c6, g)
+if __name__ == "__main__":
+    args = parse_arguments()
+    client = Client(args.app, args.exp_time, args.server_ip, args.server_port, args.target_fps, args.pixel_ip)
+    client.run()
+    client.log_results()
